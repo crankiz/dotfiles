3a251a76743309d65dc86ff8ba4cc09b0e073cbf
crankiz <svedberg.tobias@gmail.com>
2024-03-12 10:01:16 +0100

    Update windows_terminal_install.sh

--- windows/terminal/windows_terminal_install.sh
+++ windows/terminal/windows_terminal_install.sh
@@ -3,4 +3,4 @@
 windowsUserProfile=/mnt/c/Users/$(cmd.exe /c "echo %USERNAME%" 2>/dev/null | tr -d '\r')
 
 # Copy Windows Terminal settings
-cp ~/.dotfiles/windows/terminal-settings.json ${windowsUserProfile}/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.jso
+cp ~/.dotfiles/windows/terminal-settings.json ${windowsUserProfile}/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
